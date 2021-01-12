8482fd7104a1e9ba698a98e7c46fe5480bd0150f
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-01-12 11:43:22 -0500

    Add completed_at

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,12 @@ class ItemsController < ApplicationController
     redirect_to items_path
   end
 
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to items_path
+  end
+
   private
 
   def item_params
